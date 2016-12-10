129b22b60868db69d6c9996456fd5aef44a5f5a9
Jan <jan@coded-aesthetics.com>
2016-12-10 18:29:41 +0000

    cleanup

--- neard-explorenfc-1.2/examples/basic.c
+++ neard-explorenfc-1.2/examples/basic.c
@@ -23,7 +23,6 @@
 #include <stdio.h>
 #include <vlc/vlc.h>
 #include <unistd.h>
-//#include <fmod.h>
 
 #include "neardal.h"
 
@@ -39,15 +38,8 @@ struct params
 	gchar* adapterObjectPath;
  	GMainLoop* pMainLoop;
 	gint returnCode;
-	neardal_record* pRcd;
-	gchar* writeMessage;
-	gchar* messageType;
-	gchar* language;
-	gboolean copy;
-	gboolean isCopied;	
 };
 typedef struct params params_t;
-
 
 static gint start_polling(params_t* pParams)
 {
@@ -97,7 +89,6 @@ static gint start_polling(params_t* pParams)
 			{
 				g_warning("Error %d when trying to power adapter %s (%s)\r\n", err, pAdapter->name, neardal_error_get_text(err));
 			}
-//        inst = libvlc_new (0, NULL);
 		}
 
 		if( pAdapter->polling == 0 )
@@ -129,242 +120,58 @@ static gint start_polling(params_t* pParams)
 
 	return 0; //OK
 }
+
+/* event handler callback function */
 void event_man(const struct libvlc_event_t  * event, void *data)
 {
-g_printf("what");
+   g_printf("caught event");
    // g_printf( "type %s %s", event->type, libvlc_event_type_name (event->type));
 }
 
-static void play_song(char* filename) { //, FMOD_SYSTEM *system) {
+static void play_song(char* filename) { 
      libvlc_media_t *m;
-     //gboolean isPlaying;
-          const char *const opts[] = {"--aout=alsa"};
-     /* Load the VLC engine */
+     
      if (inst == NULL) {
+        /* Load the VLC engine if not previously initialized */
+        const char *const opts[] = {"--aout=alsa"};
         inst = libvlc_new (1,  opts); 
-         }
-     if (mp != NULL) {
- 	libvlc_media_player_stop (mp);
-        /* Free the media_player */
-        //libvlc_media_player_release (mp);
-
-        g_printf("%s %s", filename, currentSong);  
-        if(strcmp(filename, currentSong) == 0) {
-          strcpy(currentSong, "");
-          return;
-        }
      }
      
+     if (mp != NULL) {
+ 	libvlc_media_player_pause (mp);
+
+        if(strcmp(filename, currentSong) == 0) {
+          /* if requested song is that same that is currently playing toggle pause */
+          //strcpy(currentSong, ""); // this needs to be done if sound is to be stopped rather than paused
+          g_printf("Toggling pause on %s\n", currentSong);
+          return;
+        }
+        /* Free the media_player */
+        libvlc_media_player_release (mp);
+     }
+
+     /* register new song that is playing */
      strcpy(currentSong, filename);      
 
-     //FMOD_CHANNEL *channel = 0;
-
-     //float* vol;
-
-     //FMOD_Channel_GetVolume(channel, vol);
-
-     //g_printf("test %f", vol);
-
-     //FMOD_Channel_SetVolume(channel, 1.000);
-     //FMOD_Channel_GetVolume(channel, vol);
-
-     //g_printf("test %f", vol);
-
-     //FMOD_System_Init(system, 1, FMOD_INIT_NORMAL, NULL);     
-     //g_printf("\n%s\n", filename);
      char basedir[128] = "file:///home/pi/songs/";
-     /* Create a new item */
-     //g_printf("\nTESTESTEST\n");
      strcat(basedir, filename);
 
-     g_printf("\nNow playing: %s\n", basedir);
+     g_printf("Now playing: %s\n", basedir);
 
      m = libvlc_media_new_location (inst, basedir);
-     //m = libvlc_media_new_path (inst, "/path/to/test.mov");
-     
-     //int pid = execl("/usr/bin/omxplayer", "/usr/bin/omxplayer",  "/home/pi/tungg.mp3", (char*) NULL);
-     
-     //g_printf("%i", pid);
 
-     //FMOD_SOUND *dooropen;
-
-     //FMOD_RESULT resultat7 = FMOD_System_CreateStream(system, "/home/pi/tungg.mp3",FMOD_CREATESTREAM | FMOD_LOOP_OFF | FMOD_2D, 0, &dooropen);
-
-     //FMOD_System_PlaySound(system, dooropen, 0, false, &channel);
-
-     /* Create a media player playing environement */
+     /* Create a media player playing environment */
      mp = libvlc_media_player_new_from_media (m);
-    //man = libvlc_media_player_event_manager( mp );
-    //libvlc_event_attach(man,libvlc_MediaPlayerEndReached ,event_man,NULL);
+     /* been unsuccesfully trying to listen to end of playback event with the following lines */
+     //man = libvlc_media_player_event_manager( mp );
+     //libvlc_event_attach(man,libvlc_MediaPlayerEndReached ,event_man,NULL);
 
      /* No need to keep the media now */
      libvlc_media_release (m);
 
      /* play the media_player */
      libvlc_media_player_play (mp);
-    
-
-//     params->isPlaying = TRUE;
-    // sleep (100); /* Let it play a bit */
-     	/* Stop playing */
-    // 	 libvlc_media_player_stop (mp);
-
-     	/* Free the media_player */
-    // 	libvlc_media_player_release (mp);
-
-    
-     //libvlc_release (inst);
- 
  }
-
-
-static void dump_record(neardal_record* pRecord) {
-	if( pRecord->name != NULL )
-	{
-		g_printf("Found record %s\r\n", pRecord->name);
-	}
-	else
-	{
-		g_printf("Found record\r\n");
-	}
-
-	if( pRecord->type != NULL )
-	{
-		g_printf("Record type: \t%s\r\n", pRecord->type);
-	}
-	else
-	{
-		g_printf("Unknown record type\r\n");
-	}
-
-	//Dump fields that are set
-	if( pRecord->uri != NULL )
-	{
-		g_printf("URI: \t%s\r\n", pRecord->uri);
-	}
-
-	if( pRecord->representation != NULL )
-	{
-		g_printf("Title: \t%s\r\n", pRecord->representation);
-
-                play_song(pRecord->representation);
-	}
-
-	if( pRecord->action != NULL )
-	{
-		g_printf("Action: \t%s\r\n", pRecord->action);
-	}
-
-	if( pRecord->language != NULL )
-	{
-		g_printf("Language: \t%s\r\n", pRecord->language);
-	}
-
-	if( pRecord->encoding != NULL )
-	{
-		g_printf("Encoding: \t%s\r\n", pRecord->encoding);
-	}
-
-	if( pRecord->mime != NULL )
-	{
-		g_printf("MIME type: \t%s\r\n", pRecord->mime);
-	}
-
-	if( pRecord->uriObjSize > 0 )
-	{
-		g_printf("URI object size: \t%u\r\n", pRecord->uriObjSize);
-	}
-
-	if( pRecord->carrier != NULL )
-	{
-		g_printf("Carrier: \t%s\r\n", pRecord->carrier);
-	}
-
-	if( pRecord->ssid != NULL )
-	{
-		g_printf("SSID: \t%s\r\n", pRecord->ssid);
-	}
-
-	if( pRecord->passphrase != NULL )
-	{
-		g_printf("Passphrase: \t%s\r\n", pRecord->passphrase);
-	}
-
-	if( pRecord->encryption != NULL )
-	{
-		g_printf("Encryption: \t%s\r\n", pRecord->encryption);
-	}
-
-	if( pRecord->authentication != NULL )
-	{
-		g_printf("Authentication: \t%s\r\n", pRecord->authentication);
-	}
-}
-
-static gchar* bytes_to_str(GBytes* bytes)
-{
-	gchar* str = g_malloc0( 2*g_bytes_get_size(bytes) + 1 );
-	const guint8* data = g_bytes_get_data(bytes, NULL);
-	for(int i = 0 ; i < g_bytes_get_size(bytes); i++)
-	{
-		sprintf(&str[2*i], "%02X", data[i]);
-	}
-	return str;
-}
-
-
-
-static void dump_tag(neardal_tag* pTag)
-{
-	if( pTag->iso14443aAtqa != NULL )
-	{
-		gchar *str = bytes_to_str(pTag->iso14443aAtqa);
-		g_printf("ISO14443A ATQA: \t%s\r\n", str);
-		g_free(str);
-	}
-
-	if( pTag->iso14443aSak != NULL )
-	{
-		gchar *str = bytes_to_str(pTag->iso14443aSak);
-		g_printf("ISO14443A SAK: \t%s\r\n", str);
-		g_free(str);
-	}
-
-	if( pTag->iso14443aUid != NULL )
-	{
-		gchar *str = bytes_to_str(pTag->iso14443aUid);
-		g_printf("ISO14443A UID: \t%s\r\n", str);
-		g_free(str);
-	}
-
-	if( pTag->felicaManufacturer != NULL )
-	{
-		gchar *str = bytes_to_str(pTag->felicaManufacturer);
-		g_printf("Felica Manufacturer: \t%s\r\n", str);
-		g_free(str);
-	}
-
-	if( pTag->felicaCid != NULL )
-	{
-		gchar *str = bytes_to_str(pTag->felicaCid);
-		g_printf("Felica CID: \t%s\r\n", str);
-		g_free(str);
-	}
-
-	if( pTag->felicaIc != NULL )
-	{
-		gchar *str = bytes_to_str(pTag->felicaIc);
-		g_printf("Felica IC: \t%s\r\n", str);
-		g_free(str);
-	}
-
-	if( pTag->felicaMaxRespTimes != NULL )
-	{
-		gchar *str = bytes_to_str(pTag->felicaMaxRespTimes);
-		g_printf("Felica Maximum response times: \t%s\r\n", str);
-		g_free(str);
-	}
-}
 
 static void record_found(const char *recordName, void *pUserData)
 {
@@ -381,114 +188,10 @@ static void record_found(const char *recordName, void *pUserData)
 	}
 
 	//Dump record's content
-	dump_record(pRecord);
-	if(pParams->copy && !(pParams->isCopied))
-	{
-		if(strcmp(pRecord->type,"Text") != 0 &&
-		   strcmp(pRecord->type,"URI") != 0 &&
-//		   strcmp(pRecord->type,"HandoverCarrier") != 0 &&
-//		   strcmp(pRecord->type,"HandoverRequest") != 0 &&
-//		   strcmp(pRecord->type,"HandoverSelect") != 0 &&
-		   strcmp(pRecord->type,"SmartPoster") != 0)
-		{
-			g_printf("Not a valid record type for write (currently only 'Text', 'URI' or 'SmartPoster' are supported)\r\n");
-			exit(1);
-		}
-		if( pParams->pRcd != NULL )
-		{
-			neardal_free_record(pParams->pRcd);
-		}
-
-		pParams->pRcd = pRecord;
-		pParams->isCopied = TRUE;
-		g_printf("Record is stored\r\n");
+        if( pRecord->representation != NULL ) {
+            play_song(pRecord->representation);
 	}
-	else
-	{
-		neardal_free_record(pRecord);
-	}
-}
-
-
-
-static void tag_found(const char *tagName, void *pUserData)
-{
-	g_printf("Tag found\r\n");
-        //FMOD_SYSTEM *system;
-        //FMOD_System_Create(&system);
-        
-	errorCode_t	err;
-	neardal_tag* pTag;
-	params_t* pParams = (params_t*) pUserData;
-
-	err = neardal_get_tag_properties(tagName, &pTag);
-	if(err != NEARDAL_SUCCESS)
-	{
-		g_warning("Error %d when reading tag %s (%s)\r\n", err, tagName, neardal_error_get_text(err));
-		return;
-	}
-
-	if(pParams->writeMessage != NULL || (pParams->copy && pParams->isCopied))
-	{
-		if(pTag->readOnly)
-		{
-			g_printf("Tag is set to read only\r\n");
-			return;
-		}
-
-		if(pParams->pRcd->name != NULL)
-		{
-			g_free(pParams->pRcd->name);
-		}
-		pParams->pRcd->name = g_strdup(tagName);
-		err = neardal_tag_write(pParams->pRcd);
-		if(err != NEARDAL_SUCCESS)
-		{
-			g_warning("Error %d when writing tag %s (%s)\r\n", err, tagName, neardal_error_get_text(err));
-			return;
-		}
-		g_printf("Record was written successfully\r\n");
-	}
-	else
-	{
-		//Dump record's content
-		dump_tag(pTag);
-	}
-	neardal_free_tag(pTag);
-}
-
-static void device_found(const char *devName, void *pUserData)
-{
-	g_printf("Device found\r\n");
-	errorCode_t err;
-	neardal_dev* pDev;
-	params_t* pParams = (params_t*) pUserData;
-
-	err = neardal_get_dev_properties(devName, &pDev);
-	if(err != NEARDAL_SUCCESS)
-	{
-		g_warning("Error %d when reading device %s (%s)\r\n", err, devName, neardal_error_get_text(err));
-		return;
-	}
-
-	neardal_free_device(pDev);
-
-	if(pParams->writeMessage != NULL || (pParams->copy && pParams->isCopied))
-	{
-		if(pParams->pRcd->name != NULL)
-		{
-			g_free(pParams->pRcd->name);
-		}
-		pParams->pRcd->name = g_strdup(devName);
-		err = neardal_dev_push(pParams->pRcd);
-		if(err != NEARDAL_SUCCESS)
-		{
-			g_warning("Error %d when pushing to device %s (%s)\r\n", err, devName, neardal_error_get_text(err));
-			return;
-		}
-		g_printf("Push to device was successful\r\n");
-	}
-
+	neardal_free_record(pRecord);
 }
 
 static void tag_device_lost(const char *name, void *pUserData)
@@ -510,7 +213,6 @@ static void tag_device_lost(const char *name, void *pUserData)
 	}
 }
 
-
 int main(int argc, char** argv)
 {
 	params_t params;
@@ -519,28 +221,13 @@ int main(int argc, char** argv)
 	params.adapterObjectPath = NULL;
 	params.returnCode = 0;
 
-	params.pRcd = NULL;
-	params.writeMessage = NULL;
-	params.messageType = "";
-	params.language = "en";
-	params.copy = FALSE;
-	params.isCopied = FALSE;
-	
-               //isPlaying = FALSE;
-	//params.isPlaying = FALSE;
-       
-
 	//Parse options
 	const GOptionEntry entries[] =
 	{
-	 { "debug", 'd', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &params.debug, "Enable debugging mode", NULL },
-	 { "keep-polling", 'k', 0, G_OPTION_ARG_NONE, &params.keepPolling, "Keep polling", NULL },
-	 { "adapter", 'a', 0, G_OPTION_ARG_STRING, &params.adapterObjectPath, "Use a specific adapter", NULL},
-	 { "write", 'w', 0, G_OPTION_ARG_STRING, &params.writeMessage, "Message to write", NULL},
-	 { "type", 't', 0, G_OPTION_ARG_STRING, &params.messageType, "Text, URI,...", "Text"},
-	 { "copy", 'c', 0, G_OPTION_ARG_NONE, &params.copy, "Copy record (use with -k)", NULL},
-	 { "language", 'l', 0, G_OPTION_ARG_STRING, &params.language, "Language of record", "en"},
-	 { NULL }
+	  { "debug", 'd', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &params.debug, "Enable debugging mode", NULL },
+	  { "keep-polling", 'k', 0, G_OPTION_ARG_NONE, &params.keepPolling, "Keep polling", NULL },
+	  { "adapter", 'a', 0, G_OPTION_ARG_STRING, &params.adapterObjectPath, "Use a specific adapter", NULL},
+	  { NULL }
 	};
 
 	GOptionContext* pContext = g_option_context_new(NULL);
@@ -567,51 +254,8 @@ int main(int argc, char** argv)
 		g_setenv("G_MESSAGES_DEBUG", "all", 1);
 	}
 
-	if(params.writeMessage != NULL)	//references: https://github.com/connectivity/neardal/blob/master/lib/neardal.h
-	{
-		params.pRcd = g_malloc(sizeof(neardal_record));
-		if(params.copy)
-		{
-			g_printf("Combination of copy and write not possible\r\n");
-			return 1;
-		}
-		if(strcmp(params.messageType,"") == 0)
-		{
-			g_printf("Please specify record type using '-t' 'Text' or 'URI'\r\n");
-			return 1;			
-		}
-		if(strcmp(params.messageType,"Text") != 0 &&
-		   strcmp(params.messageType,"URI") != 0 &&
-//		   strcmp(params.messageType,"HandoverCarrier") != 0 &&
-//		   strcmp(params.messageType,"HandoverRequest") != 0 &&
-//		   strcmp(params.messageType,"HandoverSelect") != 0 &&
-		   strcmp(params.messageType,"SmartPoster") != 0)
-		{
-			g_printf("Not a valid record type for write (currently only 'Text', 'URI' or 'SmartPoster' are supported)\r\n");
-			return 1;
-		}
-		params.pRcd->name = NULL;
-		params.pRcd->action = g_strdup("Save"); //Save, Edit, Download
-		params.pRcd->carrier = g_strdup("Bluetooth"); //Bluetooth
-		params.pRcd->encoding = g_strdup("UTF-8");
-		params.pRcd->language = g_strdup(params.language); //ISO/IANA: en, jp, etc.
-		params.pRcd->mime = g_strdup("application/vnd.wfa.wsc"); //audio/mp3 //text/plain
-		params.pRcd->representation = g_strdup(params.writeMessage);
-		params.pRcd->type = g_strdup(params.messageType);
-		params.pRcd->ssid = g_strdup("vt"); //params.writeMessage;
-		params.pRcd->passphrase = g_strdup(" ");
-		params.pRcd->encryption = g_strdup("NONE"); //NONE,WEP,TKIP,AES
-		params.pRcd->authentication = g_strdup("OPEN"); //OPEN,WPA-Personal,Shared,WPA-Enterprise,WPA2-Enterprise,WPA2-Personal
-		params.pRcd->uri = params.writeMessage;//"http://www.nxp.com";
-		params.pRcd->uriObjSize = 0;//The size of the object pointed by the URI, not the URI's length
-	}
-
 	//Initialize GLib main loop
 	params.pMainLoop = g_main_loop_new(NULL, FALSE);
-
-	//Add tag found callback
-	//neardal_set_cb_tag_found(tag_found, (gpointer)&params);
-	//neardal_set_cb_dev_found(device_found, (gpointer)&params);
 
 	//Add record found callback
 	neardal_set_cb_record_found(record_found, (gpointer)&params);
@@ -635,10 +279,6 @@ int main(int argc, char** argv)
 	{
 		g_free(params.adapterObjectPath);
 	}
-	if(params.pRcd != NULL)
-	{
-		neardal_free_record(params.pRcd);
-	}
-
+	
 	return params.returnCode;
 }
